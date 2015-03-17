ba7ad3e5eadaf7162a8c5ae2442d68e38487e0ef
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-03-17 11:25:35 +0100

    Aggiunto il bluetooth. Modifica del file manifest per il bluetooth.

---
+++ app/src/main/java/it/unina/is2project/sensorgames/bluetooth/Constants.java
@@ -0,0 +1,19 @@
+package it.unina.is2project.sensorgames.bluetooth;
+
+public interface Constants {
+
+    // Message types sent from the BluetoothService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Message types
+    public static final int MSG_TYPE_DATA = 6;
+    public static final int MSG_TYPE_SYNC = 7;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+}
---
+++ app/src/main/java/it/unina/is2project/sensorgames/bluetooth/Serializer.java
@@ -0,0 +1,43 @@
+package it.unina.is2project.sensorgames.bluetooth;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Serializer {
+
+    public Serializer(){}
+
+    public static byte[] serializeObject(Object o) {
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = null;
+        try {
+            os = new ObjectOutputStream(out);
+            os.writeObject(o);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toByteArray();
+    }
+
+    public static Object deserializeObject(byte[] b) {
+
+        ByteArrayInputStream in = new ByteArrayInputStream(b);
+        ObjectInputStream is = null;
+        Object object = null;
+        try {
+            is = new ObjectInputStream(in);
+            object = is.readObject();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+}
