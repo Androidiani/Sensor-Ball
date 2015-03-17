82f80c59ea631a0535fd2d8bcc32f12d6750b487
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-03-17 18:31:10 +0100

    Aggiunto onCreateOptionsMenu nella classe TwoPlayerActivity

--- app/src/main/java/it/unina/is2project/sensorgames/TwoPlayerActivity.java
+++ app/src/main/java/it/unina/is2project/sensorgames/TwoPlayerActivity.java
@@ -2,6 +2,7 @@ package it.unina.is2project.sensorgames;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
 
 public class TwoPlayerActivity extends ActionBarActivity {
 
@@ -9,5 +10,10 @@ public class TwoPlayerActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2p);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
