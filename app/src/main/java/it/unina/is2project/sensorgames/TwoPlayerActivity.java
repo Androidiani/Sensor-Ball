2923e507c8469aeac4b0f0fa3e2aa3128085a081
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-03-17 18:26:14 +0100

    Aggiunta classe "TwoPlayerActivity" Ultimato layout di activity_2p Aggiunto XML del menù dell'activity2p

---
+++ app/src/main/java/it/unina/is2project/sensorgames/TwoPlayerActivity.java
@@ -0,0 +1,13 @@
+package it.unina.is2project.sensorgames;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class TwoPlayerActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2p);
+    }
+}
