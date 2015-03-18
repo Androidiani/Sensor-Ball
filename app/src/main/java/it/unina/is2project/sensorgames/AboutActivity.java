e454aa076b1e08d60764435805e5b51616a37087
helios2 <mariglianofrancesco@gmail.com>
2015-03-18 17:40:07 +0100

    Modifica all'activity "About Us"

--- app/src/main/java/it/unina/is2project/sensorgames/AboutActivity.java
+++ app/src/main/java/it/unina/is2project/sensorgames/AboutActivity.java
@@ -1,18 +1,43 @@
 package it.unina.is2project.sensorgames;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 
 public class AboutActivity extends ActionBarActivity {
+
+    // Font typeface
+    private Typeface typeFace;
+
+    private TextView titolo;
+    private TextView descrizione;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        /** Set the fullscreen window */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        /** Load the font */
+        typeFace = Typeface.createFromAsset(getAssets(),"font/secrcode.ttf");
+
+        /** Find view */
+        titolo = (TextView) findViewById(R.id.lblNameApp);
+        descrizione = (TextView) findViewById(R.id.lblDescription);
+
+        /** Set font */
+        titolo.setTypeface(typeFace);
+        descrizione.setTypeface(typeFace);
     }
 
     @Override
