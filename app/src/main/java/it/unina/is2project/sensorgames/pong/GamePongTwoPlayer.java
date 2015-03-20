0ce2e779f00dbcedfebc3bce442a40418bdf1829
Gabriele <gabriele.saviano91@gmail.com>
2015-03-20 17:16:16 +0100

    Aggiunta classe GamePongTwoPlayer

---
+++ app/src/main/java/it/unina/is2project/sensorgames/pong/GamePongTwoPlayer.java
@@ -0,0 +1,25 @@
+package it.unina.is2project.sensorgames.pong;
+
+
+public class GamePongTwoPlayer extends GamePong {
+
+    @Override
+    public void settingPhysics() {
+
+    }
+
+    @Override
+    public void restartOnBallLost() {
+
+    }
+
+    @Override
+    public void addScore() {
+
+    }
+
+    @Override
+    public void remScore() {
+
+    }
+}
