02cf32260428abb4a7d61ddd16695563aed911d5
helios2 <mariglianofrancesco@gmail.com>
2015-03-23 11:59:11 +0100

    Aggiunte le modalità training

--- app/src/main/java/it/unina/is2project/sensorgames/pong/GamePongTraining.java
+++ app/src/main/java/it/unina/is2project/sensorgames/pong/GamePongTraining.java
@@ -1,19 +1,162 @@
 package it.unina.is2project.sensorgames.pong;
 
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.region.ITextureRegion;
+
+import it.unina.is2project.sensorgames.R;
+
+import static org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory.createFromResource;
 
 public class GamePongTraining extends GamePong {
+
+    // Settings button
+    protected BitmapTextureAtlas settingsTexture;
+    protected ITextureRegion settingsTextureRegion;
+    protected Sprite settingsSprite;
+
+    // Easy mode button
+    protected BitmapTextureAtlas easyTexture;
+    protected ITextureRegion easyTextureRegion;
+    protected Sprite easySprite;
+
+    // Normal mode button
+    protected BitmapTextureAtlas normalTexture;
+    protected ITextureRegion normalTextureRegion;
+    protected Sprite normalSprite;
+
+    // Insane mode button
+    protected BitmapTextureAtlas insaneTexture;
+    protected ITextureRegion insaneTextureRegion;
+    protected Sprite insaneSprite;
+
+    // Game's mode
+    private static final int EASY_MODE = 0;
+    private static final int NORMAL_MODE = 1;
+    private static final int INSANE_MODE = 2;
+
+    // Events
+    private boolean settingsPause = false;
+    private boolean enableModes = false;
+
+    // Speed directions
+    private Point DIRECTIONS;
 
     @Override
     protected Scene onCreateScene() {
         super.onCreateScene();
 
+        /** Adding the easySprite to the scene */
+        easySprite = new Sprite(0, 0, easyTextureRegion, getVertexBufferObjectManager()){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+                if(enableModes){
+                    Log.d("","Easy mode");
+                    setMode(EASY_MODE);
+                }
+                return true;
+            }
+        };
+        easySprite.setWidth(CAMERA_WIDTH*0.5f);
+        easySprite.setHeight(easySprite.getWidth()*0.581f); // 0.581 is the rate between image width and height
+        easySprite.setX((CAMERA_WIDTH - easySprite.getWidth())/2); // Position set after scaling
+        easySprite.setY(((CAMERA_HEIGHT - easySprite.getHeight())/2) - easySprite.getHeight());
+        scene.registerTouchArea(easySprite);
+
+        /** Adding the normalSprite to the scene */
+        normalSprite = new Sprite(0, 0, normalTextureRegion, getVertexBufferObjectManager()){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+                if(enableModes){
+                    Log.d("","Normal mode");
+                    setMode(NORMAL_MODE);
+                }
+                return true;
+            }
+        };
+        normalSprite.setWidth(CAMERA_WIDTH*0.5f);
+        normalSprite.setHeight(normalSprite.getWidth()*0.581f); // 0.581 is the rate between image width and height
+        normalSprite.setX((CAMERA_WIDTH - normalSprite.getWidth())/2); // Position set after scaling
+        normalSprite.setY((CAMERA_HEIGHT - normalSprite.getHeight())/2);
+        scene.registerTouchArea(normalSprite);
+
+        /** Adding the insaneSprite to the scene */
+        insaneSprite = new Sprite(0, 0, insaneTextureRegion, getVertexBufferObjectManager()){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+                if(enableModes){
+                    Log.d("","Insane mode");
+                    setMode(INSANE_MODE);
+                }
+                return true;
+            }
+        };
+        insaneSprite.setWidth(CAMERA_WIDTH*0.5f);
+        insaneSprite.setHeight(insaneSprite.getWidth()*0.581f); // 0.581 is the rate between image width and height
+        insaneSprite.setX((CAMERA_WIDTH - insaneSprite.getWidth())/2); // Position set after scaling
+        insaneSprite.setY(((CAMERA_HEIGHT - insaneSprite.getHeight())/2) + insaneSprite.getHeight());
+        scene.registerTouchArea(insaneSprite);
+
+        /** Adding the settingsSprite to the scene */
+        settingsSprite = new Sprite(0, 0, settingsTextureRegion, getVertexBufferObjectManager()){
+            @Override
+            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+                if(!settingsPause){ // Can't touch settings again
+                    showMenu();
+                    settingsPause = true;
+                    enableModes = true;
+                }
+                return true;
+            }
+        };
+        settingsSprite.setWidth(CAMERA_WIDTH*0.1f);
+        settingsSprite.setHeight(CAMERA_WIDTH * 0.1f);
+        settingsSprite.setX(CAMERA_WIDTH - settingsSprite.getWidth()); // Position set after scaling
+        scene.attachChild(settingsSprite);
+        scene.registerTouchArea(settingsSprite);
+
         /** Setting up the physics of the game */
         settingPhysics();
 
         return scene;
+    }
+
+
+    @Override
+    protected void loadGraphics() {
+        super.loadGraphics();
+
+        /** Settings button */
+        Drawable settingsDrawable = getResources().getDrawable(R.drawable.menu_button_settings);
+        settingsTexture = new BitmapTextureAtlas(getTextureManager(), settingsDrawable.getIntrinsicWidth(), settingsDrawable.getIntrinsicHeight());
+        settingsTextureRegion = createFromResource(settingsTexture, this,R.drawable.menu_button_settings , 0, 0);
+        settingsTexture.load();
+
+        /** Easy mode */
+        Drawable easyDrawable = getResources().getDrawable(R.drawable.training_setting_easy);
+        easyTexture = new BitmapTextureAtlas(getTextureManager(), easyDrawable.getIntrinsicWidth(), easyDrawable.getIntrinsicHeight());
+        easyTextureRegion = createFromResource(easyTexture, this,R.drawable.training_setting_easy , 0, 0);
+        easyTexture.load();
+
+        /** Normal mode */
+        Drawable normalDrawable = getResources().getDrawable(R.drawable.training_setting_normal);
+        normalTexture = new BitmapTextureAtlas(getTextureManager(), normalDrawable.getIntrinsicWidth(), normalDrawable.getIntrinsicHeight());
+        normalTextureRegion = createFromResource(normalTexture, this,R.drawable.training_setting_normal , 0, 0);
+        normalTexture.load();
+
+        /** Insane mode */
+        Drawable insaneDrawable = getResources().getDrawable(R.drawable.training_setting_insane);
+        insaneTexture = new BitmapTextureAtlas(getTextureManager(), insaneDrawable.getIntrinsicWidth(), insaneDrawable.getIntrinsicHeight());
+        insaneTextureRegion = createFromResource(insaneTexture, this,R.drawable.training_setting_insane , 0, 0);
+        insaneTexture.load();
     }
 
     @Override
@@ -119,5 +262,87 @@ public class GamePongTraining extends GamePong {
     @Override
     public void remScore() {
         //do nothing
+    }
+
+    /**
+     * Show the mode's menù
+     */
+    private void showMenu(){
+        // Store directions data
+        DIRECTIONS = getDirections();
+        Log.d("","Directions:" + DIRECTIONS.x + " " + DIRECTIONS.y);
+
+        // Stop the ball
+        handler.setVelocity(0f);
+        GAME_VELOCITY = 0;
+
+        // Attach the menu's children
+        scene.attachChild(easySprite);
+        scene.attachChild(normalSprite);
+        scene.attachChild(insaneSprite);
+
+    }
+    /**
+     * Hide the mode's menù
+     */
+    private void hideMenu(){
+        // Detach menu's children
+        scene.detachChild(easySprite);
+        scene.detachChild(normalSprite);
+        scene.detachChild(insaneSprite);
+    }
+    /**
+     * Set the game's mode
+     */
+    private void setMode(int mode){
+        switch (mode) {
+            case EASY_MODE: {
+                handler.setVelocityX(BALL_SPEED*DIRECTIONS.x);
+                handler.setVelocityY(BALL_SPEED*DIRECTIONS.y);
+                GAME_VELOCITY = 2;
+                barSprite.setWidth(CAMERA_WIDTH*0.3f);
+                break;
+            }
+
+            case NORMAL_MODE: {
+                handler.setVelocityX(BALL_SPEED*2*DIRECTIONS.x);
+                handler.setVelocityY(BALL_SPEED*2*DIRECTIONS.y);
+                GAME_VELOCITY = 4;
+                barSprite.setWidth(CAMERA_WIDTH*0.3f);
+                break;
+            }
+
+            case INSANE_MODE: {
+                handler.setVelocityX(BALL_SPEED*3*DIRECTIONS.x);
+                handler.setVelocityY(BALL_SPEED*3*DIRECTIONS.y);
+                GAME_VELOCITY = 4;
+                barSprite.setWidth(CAMERA_WIDTH*0.2f);
+                break;
+            }
+        }
+        hideMenu();
+        settingsPause = false;
+        enableModes = false;
+    }
+
+    /**
+     * Get the directions of the ball
+     * @return
+     */
+    private Point getDirections(){
+        Point mPoint = new Point();
+        if(handler.getVelocityX() < 0 && handler.getVelocityY() < 0){
+            mPoint.set(-1,-1);
+        }
+        if(handler.getVelocityX() < 0 && handler.getVelocityY() > 0){
+            mPoint.set(-1,1);
+        }
+        if(handler.getVelocityX() > 0 && handler.getVelocityY() < 0){
+            mPoint.set(1,-1);
+        }
+        if(handler.getVelocityX() > 0 && handler.getVelocityY() > 0){
+            mPoint.set(1,1);
+        }
+        return mPoint;
     }
 }
