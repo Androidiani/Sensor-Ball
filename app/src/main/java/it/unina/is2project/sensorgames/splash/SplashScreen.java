86dd39e377873065b237c661bd710ddf66c44ff7
helios2 <mariglianofrancesco@gmail.com>
2015-03-30 12:25:05 +0200

    Realizzata la prima versione della Splash Screen di avvio

---
+++ app/src/main/java/it/unina/is2project/sensorgames/splash/SplashScreen.java
@@ -0,0 +1,10 @@
+package it.unina.is2project.sensorgames.splash;
+
+import android.app.Activity;
+
+public class SplashScreen extends Activity {
+    protected long ms=0;
+    protected long splashTime=2000;
+    protected boolean splashActive = true;
+    protected boolean paused=false;
+}
---
+++ app/src/main/java/it/unina/is2project/sensorgames/splash/SplashScreenMain.java
@@ -0,0 +1,60 @@
+package it.unina.is2project.sensorgames.splash;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import it.unina.is2project.sensorgames.MainActivity;
+import it.unina.is2project.sensorgames.R;
+
+public class SplashScreenMain extends SplashScreen {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_main);
+
+        Thread mythread = new Thread() {
+            public void run() {
+                try {
+                    while (splashActive && ms < splashTime) {
+                        if(!paused)
+                            ms=ms+100;
+                        sleep(100);
+                    }
+                } catch(Exception e) {}
+                finally {
+                    Intent intent = new Intent(SplashScreenMain.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        mythread.start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
