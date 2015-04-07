af01c98d470a6609bcd84d0b7766ae5005d5b23a
helios2 <mariglianofrancesco@gmail.com>
2015-04-07 12:55:52 +0200

    Aggiornata la logica del Training Mode: creata una nuova activity per la gestione della velocità della palla e della barra. TODO: aggiornare gli eventi una volta ultimata la classe BonusManager

--- app/src/main/java/it/unina/is2project/sensorgames/pong/GamePongTraining.java
+++ app/src/main/java/it/unina/is2project/sensorgames/pong/GamePongTraining.java
@@ -1,5 +1,6 @@
 package it.unina.is2project.sensorgames.pong;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 
@@ -23,39 +24,18 @@ public class GamePongTraining extends GamePong {
     private ITextureRegion settingTextureRegion;
     private Sprite settingSprite;
 
-    // Easy mode button
-    private BitmapTextureAtlas easyTexture;
-    private ITextureRegion easyTextureRegion;
-    private Sprite easySprite;
-
-    // Normal mode button
-    private BitmapTextureAtlas normalTexture;
-    private ITextureRegion normalTextureRegion;
-    private Sprite normalSprite;
-
-    // Insane mode button
-    private BitmapTextureAtlas insaneTexture;
-    private ITextureRegion insaneTextureRegion;
-    private Sprite insaneSprite;
-
     // Text Hit
     private Text textHit;
 
-    // 
+    // Hit count
     private int hit_count = 0;
-    private static final int EASY_MODE = 1;
-    private static final int NORMAL_MODE = 2;
-    private static final int INSANE_MODE = 3;
 
     // Events
-    private boolean enableModes = false;
     private long secondTap;
 
     @Override
     protected Scene onCreateScene() {
         super.onCreateScene();
-
-        float spriteRate = easyTextureRegion.getHeight() / easyTextureRegion.getWidth();
 
         // Adding the textHit to the scene
         textHit = new Text(10, 10, font, "", 20, getVertexBufferObjectManager());
@@ -66,9 +46,9 @@ public class GamePongTraining extends GamePong {
         settingSprite = new Sprite(0, 0, settingTextureRegion, getVertexBufferObjectManager()) {
             @Override
             public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (!enableModes) {
-                    showMenu();
-                }
+                Intent intent = new Intent(getBaseContext(), TrainingSettings.class);
+                startActivity(intent);
+                finish();
                 return true;
             }
         };
@@ -78,67 +58,24 @@ public class GamePongTraining extends GamePong {
         scene.registerTouchArea(settingSprite);
         scene.attachChild(settingSprite);
 
-        // Adding the easySprite to the scene
-        easySprite = new Sprite(0, 0, easyTextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (enableModes) {
-                    secondTap = System.currentTimeMillis();
-                    if (secondTap - firstTap > 500) {
-                        Log.d(TAG, "Easy mode");
-                        setMode(EASY_MODE);
-                    }
-                }
-                return true;
-            }
-        };
-        easySprite.setWidth(CAMERA_WIDTH * 0.5f);
-        easySprite.setHeight(easySprite.getWidth() * spriteRate);
-        easySprite.setX((CAMERA_WIDTH - easySprite.getWidth()) / 2); // Position set after scaling
-        easySprite.setY(((CAMERA_HEIGHT - easySprite.getHeight()) / 2) - easySprite.getHeight());
-
-        // Adding the normalSprite to the scene
-        normalSprite = new Sprite(0, 0, normalTextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (enableModes) {
-                    secondTap = System.currentTimeMillis();
-                    if (secondTap - firstTap > 500) {
-                        Log.d(TAG, "Normal mode");
-                        setMode(NORMAL_MODE);
-                    }
-                }
-                return true;
-            }
-        };
-        normalSprite.setWidth(CAMERA_WIDTH * 0.5f);
-        normalSprite.setHeight(normalSprite.getWidth() * spriteRate);
-        normalSprite.setX((CAMERA_WIDTH - normalSprite.getWidth()) / 2); // Position set after scaling
-        normalSprite.setY((CAMERA_HEIGHT - normalSprite.getHeight()) / 2);
-
-        // Adding the insaneSprite to the scene
-        insaneSprite = new Sprite(0, 0, insaneTextureRegion, getVertexBufferObjectManager()) {
-            @Override
-            public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
-                if (enableModes) {
-                    secondTap = System.currentTimeMillis();
-                    if (secondTap - firstTap > 500) {
-                        Log.d(TAG, "Insane mode");
-                        setMode(INSANE_MODE);
-                    }
-                }
-                return true;
-            }
-        };
-        insaneSprite.setWidth(CAMERA_WIDTH * 0.5f);
-        insaneSprite.setHeight(insaneSprite.getWidth() * spriteRate);
-        insaneSprite.setX((CAMERA_WIDTH - insaneSprite.getWidth()) / 2); // Position set after scaling
-        insaneSprite.setY(((CAMERA_HEIGHT - insaneSprite.getHeight()) / 2) + insaneSprite.getHeight());
-
         // Setting up the physics of the game
         settingPhysics();
 
+        // Get options by training settings
+        Intent i = getIntent();
+        int ballSpeed = i.getIntExtra("ballspeed", 1);
+        int barSpeed = i.getIntExtra("barspeed", 1);
+        int event = i.getIntExtra("event", 0);
+
+        setTrainingMode(ballSpeed, barSpeed, event);
+
         return scene;
+    }
+
+    private void setTrainingMode(int ball_speed, int bar_speed, int event){
+        handler.setVelocity(ball_speed * BALL_SPEED, -ball_speed * BALL_SPEED);
+        GAME_VELOCITY = bar_speed * GAME_VELOCITY;
+        //TODO logica con event una volta creata la classe dei bonus
     }
 
 
@@ -151,24 +88,6 @@ public class GamePongTraining extends GamePong {
         settingTexture = new BitmapTextureAtlas(getTextureManager(), settingDrawable.getIntrinsicWidth(), settingDrawable.getIntrinsicHeight());
         settingTextureRegion = createFromResource(settingTexture, this, R.drawable.setting, 0, 0);
         settingTexture.load();
-
-        // Easy mode
-        Drawable easyDrawable = getResources().getDrawable(R.drawable.training_setting_easy);
-        easyTexture = new BitmapTextureAtlas(getTextureManager(), easyDrawable.getIntrinsicWidth(), easyDrawable.getIntrinsicHeight());
-        easyTextureRegion = createFromResource(easyTexture, this, R.drawable.training_setting_easy, 0, 0);
-        easyTexture.load();
-
-        // Normal mode
-        Drawable normalDrawable = getResources().getDrawable(R.drawable.training_setting_normal);
-        normalTexture = new BitmapTextureAtlas(getTextureManager(), normalDrawable.getIntrinsicWidth(), normalDrawable.getIntrinsicHeight());
-        normalTextureRegion = createFromResource(normalTexture, this, R.drawable.training_setting_normal, 0, 0);
-        normalTexture.load();
-
-        // Insane mode
-        Drawable insaneDrawable = getResources().getDrawable(R.drawable.training_setting_insane);
-        insaneTexture = new BitmapTextureAtlas(getTextureManager(), insaneDrawable.getIntrinsicWidth(), insaneDrawable.getIntrinsicHeight());
-        insaneTextureRegion = createFromResource(insaneTexture, this, R.drawable.training_setting_insane, 0, 0);
-        insaneTexture.load();
     }
 
     @Override
@@ -230,67 +149,6 @@ public class GamePongTraining extends GamePong {
         if (x <= settingSprite.getX() + settingSprite.getWidth() && x >= settingSprite.getX() && y >= settingSprite.getY() && y <= settingSprite.getY() + settingSprite.getHeight())
             checkTouchSpriteStatus = true;
         return checkTouchSpriteStatus;
-    }
-
-    /**
-     * Show the mode's menu
-     */
-    private void showMenu() {
-        // Register Touch Area
-        scene.registerTouchArea(easySprite);
-        scene.registerTouchArea(normalSprite);
-        scene.registerTouchArea(insaneSprite);
-
-        // Attach the menu's children
-        scene.attachChild(easySprite);
-        scene.attachChild(normalSprite);
-        scene.attachChild(insaneSprite);
-
-        enableModes = true;
-    }
-
-    /**
-     * Hide the mode's menu
-     */
-    private void hideMenu() {
-        // Unregister Touch Area
-        scene.unregisterTouchArea(easySprite);
-        scene.unregisterTouchArea(normalSprite);
-        scene.unregisterTouchArea(insaneSprite);
-
-        // Detach menu's children
-        scene.detachChild(easySprite);
-        scene.detachChild(normalSprite);
-        scene.detachChild(insaneSprite);
-
-        enableModes = false;
-    }
-
-    /**
-     * Set the game's mode
-     */
-    private void setMode(int mode) {
-        switch (mode) {
-            case EASY_MODE: {
-                GAME_VELOCITY = 2 * DEVICE_RATIO;
-                handler.setVelocity(BALL_SPEED, -BALL_SPEED);
-                barSprite.setWidth(CAMERA_WIDTH * 0.3f);
-                break;
-            }
-            case NORMAL_MODE: {
-                GAME_VELOCITY = 3 * DEVICE_RATIO;
-                handler.setVelocity(BALL_SPEED * 2, -BALL_SPEED * 2);
-                barSprite.setWidth(CAMERA_WIDTH * 0.21f);
-                break;
-            }
-            case INSANE_MODE: {
-                GAME_VELOCITY = 4 * DEVICE_RATIO;
-                handler.setVelocity(BALL_SPEED * 4, -BALL_SPEED * 4);
-                barSprite.setWidth(CAMERA_WIDTH * 0.15f);
-                break;
-            }
-        }
-        hideMenu();
     }
 
 }
---
+++ app/src/main/java/it/unina/is2project/sensorgames/pong/TrainingSettings.java
@@ -0,0 +1,162 @@
+package it.unina.is2project.sensorgames.pong;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import it.unina.is2project.sensorgames.R;
+
+public class TrainingSettings extends ActionBarActivity {
+
+    // Font typeface
+    private Typeface typeFace;
+
+    // Spinner
+    Spinner ball_speed_spinner;
+    Spinner bar_speed_spinner;
+
+    // TextView
+    TextView title;
+    TextView ball_speed_textview;
+    TextView bar_speed_textview;
+
+    // Array adapter
+    ArrayAdapter ball_speed_adapter;
+    ArrayAdapter bar_speed_adapter;
+
+    // Button
+    Button back;
+
+    // Data
+    private int ball_speed;
+    private int bar_speed;
+    private int event;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_settings);
+
+        /** Set the fullscreen window */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        /** Find view */
+        findView();
+
+        /** Populate spinner */
+        populateSpinner();
+
+        /** Set-up the typeface */
+        setTypeface();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), GamePongTraining.class);
+                intent.putExtra("ballspeed", ball_speed);
+                intent.putExtra("barspeed", bar_speed);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void findView(){
+        // Spinner
+        ball_speed_spinner = (Spinner) findViewById(R.id.spinner_ball_speed);
+        bar_speed_spinner = (Spinner) findViewById(R.id.spinner_bar_speed);
+
+        // TextView
+        ball_speed_textview = (TextView) findViewById(R.id.txt_ball_speed);
+        bar_speed_textview = (TextView) findViewById(R.id.txt_bar_speed);
+        title = (TextView) findViewById(R.id.title_setting_training);
+
+        // Button
+        back = (Button) findViewById(R.id.btn_back);
+    }
+
+    private void setTypeface(){
+        /** Load the font */
+        typeFace = Typeface.createFromAsset(getAssets(), "font/secrcode.ttf");
+
+        /** Set the typeface */
+        ball_speed_textview.setTypeface(typeFace);
+        bar_speed_textview.setTypeface(typeFace);
+        title.setTypeface(typeFace);
+    }
+
+    private void populateSpinner(){
+        // Ball Speed Spinner
+        ball_speed_adapter = ArrayAdapter.createFromResource(this, R.array.ball_speed_spinner, android.R.layout.simple_spinner_item);
+        ball_speed_spinner.setAdapter(ball_speed_adapter);
+        ball_speed_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ball_speed = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                ball_speed = 1;
+            }
+        });
+
+        // Bar Speed Spinner
+        bar_speed_adapter = ArrayAdapter.createFromResource(this, R.array.bar_speed_spinner, android.R.layout.simple_spinner_item);
+        bar_speed_spinner.setAdapter(bar_speed_adapter);
+        bar_speed_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                bar_speed = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                bar_speed = 1;
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_training_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // do nothing
+    }
+}
