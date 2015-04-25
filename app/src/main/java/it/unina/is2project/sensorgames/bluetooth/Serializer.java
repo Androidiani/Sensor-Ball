0312ea6e91c79929d9706257cb68486c803096a7
Gabriele <gabriele.saviano91@gmail.com>
2015-04-25 17:18:54 +0200

    Reformat code in Serializer.java

--- app/src/main/java/it/unina/is2project/sensorgames/bluetooth/Serializer.java
+++ app/src/main/java/it/unina/is2project/sensorgames/bluetooth/Serializer.java
@@ -8,7 +8,8 @@ import java.io.ObjectOutputStream;
 
 public class Serializer {
 
-    public Serializer(){}
+    public Serializer() {
+    }
 
     public static byte[] serializeObject(Object o) {
 
