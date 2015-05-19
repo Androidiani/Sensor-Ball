e77aa39eb0cc983d4e59140e4aa84e12fdeec181
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-05-19 17:39:07 +0200

    Primo cambiamento alla schermata di About

--- app/src/main/java/it/unina/is2project/sensorgames/activity/AboutActivity.java
+++ app/src/main/java/it/unina/is2project/sensorgames/activity/AboutActivity.java
@@ -3,6 +3,7 @@ package it.unina.is2project.sensorgames.activity;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -19,11 +20,10 @@ public class AboutActivity extends Activity {
         // Set the fullscreen window
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         // Find view
-        TextView titolo = (TextView) findViewById(R.id.lblNameApp);
         TextView descrizione = (TextView) findViewById(R.id.lblDescription);
         // Load font and set typeface
-        Typeface typeFace = Typeface.createFromAsset(getAssets(), "font/secrcode.ttf");
-        titolo.setTypeface(typeFace);
-        descrizione.setTypeface(typeFace);
+//        Typeface typeFace = Typeface.createFromAsset(getAssets(), "font/secrcode.ttf");
+//        descrizione.setTypeface(typeFace);
+        descrizione.setText(Html.fromHtml(getResources().getString(R.string.text_app_about_description)));
     }
 }
